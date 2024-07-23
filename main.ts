a09efae3c6fb433e2368eff2fbefe47b70df0e6b
rasurathore13 <30598607+rasurathore13@users.noreply.github.com>
2024-07-23 10:36:45 +0530

    Update main.ts with chain or responsibililty

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 //Bridge with Factory
+// Use factory and bridge when u want to switch between multiple options in any system like - 
+// If you want to switch between multiplt notifications in a notification system, 
+// If you want to switch between multiple documents in a document creation system(CSV, Excel, etc),
+// If you want to switch between multiple loggers (Console log, File log, etc)
 
 interface INotification{
     sendNotification() : void;
@@ -43,6 +47,12 @@ notificationService.sendNotification();
 
 
 // Decoraters
+// Use decorators if u want add extra functionality to controller endpoints like only let this 
+// controller hit by an authenticated requests.
+// Use decorators when u want to add extra functionality to DTOs, like only take this object if
+// the name property of this object is more than 5 chracters.
+//
+
 
 interface IService{
     performSomeOperation(statement: string) : string;
@@ -92,3 +102,62 @@ let advanceDecoratorService = new DecoratorService(advanceService);
 basicDecoratorService.performSomeOperation('Hi there');
 advanceDecoratorService.performSomeOperation('Hi there');
 
+// Chain of reponsibility
+// Lets you handle request by a chain of handlers, one after the other
+// Use it in middlewares, authentication of reuqets, error logging of requests
+
+interface IHandler {
+    handle(data:string): void ;
+}
+
+class FirstHandler implements IHandler{
+    
+    private nextHandler :IHandler | null;
+
+    constructor(nextHandler: IHandler | null) {
+        this.nextHandler = nextHandler;
+    } 
+
+    handle(data: string): void{
+        console.log("Performing some action from 1st handler -> "+data)
+        if(this.nextHandler == undefined || this.nextHandler == null) {
+            return
+        }
+        this.nextHandler.handle(data);
+    }
+}
+
+class SecondHandler implements IHandler{
+    private nextHandler: IHandler | null;
+    constructor(nextHandler: IHandler|null) {
+        this.nextHandler = nextHandler;
+    }
+    handle(data: string): void{
+        console.log("Handling from the 2nd handler"+data);
+        if(this.nextHandler==null||this.nextHandler==undefined){
+            return 
+        }
+        this.nextHandler.handle(data);
+    }
+}
+
+class ThridHandler implements IHandler{
+    private nextHandler : IHandler | null;
+
+    constructor(nextHandler: IHandler | null) {
+        this.nextHandler = nextHandler;
+    }
+
+    handle(data: string): void {
+        console.log("Handling from 3rd handler  "+ data)
+        if(this.nextHandler!=null || this.nextHandler!=undefined){
+            this.nextHandler.handle(data);
+        }
+    }
+}
+
+let thridHandler = new ThridHandler(null)
+let secondHandler = new SecondHandler(thridHandler)
+let firstHandler = new FirstHandler(secondHandler);
+
+firstHandler.handle("Test Data");
